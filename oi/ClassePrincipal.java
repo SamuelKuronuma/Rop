5aa5ccca07b128587c16488e1720842728b2808e
SamuelKuronuma <samuel.kuronuma@etec.sp.gov.br>
2025-05-30 10:46:11 -0300

    Adicionados outros métodos

--- oi/ClassePrincipal.java
+++ oi/ClassePrincipal.java
@@ -7,13 +7,22 @@ public class ClassePrincipal {
 		classeAlunos mikaelObj = new classeAlunos();
 		classeAlunos abraaoObj = new classeAlunos();
 		
-		mikaelObj.nomeAt = "Mikael";
-		mikaelObj.cursoAt = "Informática";
-		mikaelObj.notaAt = 8.5F;
+		//mikaelObj.nomeAt = "Mikael";
+		//mikaelObj.cursoAt = "Informática";
+		//mikaelObj.notaAt = 8.5F;
+		
+		mikaelObj.receberNomeMet("Mikael");
+		mikaelObj.receberCursoMet("Informática");
+		mikaelObj.receberNotaMet(8.5F);
 
-		System.out.println(mikaelObj.nomeAt);
-		System.out.println(mikaelObj.cursoAt);
-		System.out.println(mikaelObj.notaAt);
+		//System.out.println(mikaelObj.nomeAt);
+		//System.out.println(mikaelObj.cursoAt);
+		//System.out.println(mikaelObj.notaAt);
+		
+		mikaelObj.mostrarNomeMet();
+		mikaelObj.mostrarCursoMet();
+		mikaelObj.mostrarNotaMet();
+		
 	}
 
 }
--- oi/classeAlunos.java
+++ oi/classeAlunos.java
@@ -4,4 +4,23 @@ public class classeAlunos {
 	String nomeAt;
 	float notaAt;
 	String cursoAt;
+	
+	public void mostrarNomeMet() {
+		System.out.println(this.nomeAt);
+	}
+	public void mostrarCursoMet() {
+		System.out.println(this.cursoAt);
+	}
+	public void mostrarNotaMet() {
+		System.out.println(this.notaAt);
+	}
+	public void receberNomeMet (String nomePar) {
+		this.nomeAt = nomePar;
+	}
+	public void receberCursoMet (String cursoPar) {
+		this.cursoAt = cursoPar;
+	}
+	public void receberNotaMet (float notaPar) {
+		this.notaAt = notaPar;
+	}
 }
